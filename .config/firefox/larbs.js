5ecb2223c519a350c4aeb1bca88a8dc76c2b0380
HelionSmoker <gritcodragos04@gmail.com>
2024-06-05 09:20:26 +0300

    Add my firefox config

--- .config/firefox/larbs.js
+++ .config/firefox/larbs.js
@@ -6,27 +6,26 @@ user_pref("browser.urlbar.quicksuggest.enabled", false);
 user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 
 // Do not suggest web history in the URL bar:
-user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.history", true);
 
 // Do not prefil forms:
 user_pref("signon.prefillForms", false);
 
 // Do not autocomplete in the URL bar:
-user_pref("browser.urlbar.autoFill", false);
+user_pref("browser.urlbar.autoFill", true);
 
 // Enable the addition of search keywords:
 user_pref("keyword.enabled", true);
 
 // Allow access to http (i.e. not https) sites:
-user_pref("dom.security.https_only_mode", false);
+user_pref("dom.security.https_only_mode", true);
 
 // Keep cookies until expiration or user deletion:
 user_pref("network.cookie.lifetimePolicy", 0);
 
-user_pref("dom.webnotifications.serviceworker.enabled", false);
-
 // Disable push notifications:
-user_pref("dom.push.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", true);
+user_pref("dom.push.enabled", true);
 
 // Disable the pocket antifeature:
 user_pref("extensions.pocket.enabled", false);
@@ -41,7 +40,7 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("network.http.referer.XOriginPolicy", 0);
 
 // Disable Firefox sync and its menu entries
-user_pref("identity.fxaccounts.enabled", false);
+user_pref("identity.fxaccounts.enabled", true);
 
 // Fix the issue where right mouse button instantly clicks
 user_pref("ui.context_menus.after_mouseup", true);
